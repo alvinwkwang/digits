e637f73e67f9df6814c5842ad9fb4bb86dd45eb9
alvinwkwang <wang.wk.alvin@gmail.com>
2013-10-15 19:44:44 -1000

    Completed Attempt-1-2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.formdata.ContactFormData;
 import views.html.Index;
 import views.html.NewContact;
 
@@ -20,10 +22,22 @@ public class Application extends Controller {
   
   /**
    * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
+   * @return The NewContact page.
    */
   public static Result newcontact() {
-    return ok(NewContact.render("Welcome to Page1."));
+    Form<ContactFormData> formData = Form.form(ContactFormData.class);
+    return ok(NewContact.render(formData));
     
+  }
+  
+  /**
+   * Processes the form submitted form the NewContact page.
+   * @return The NewContact Page.
+   */
+  public static Result postContact() {
+    Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
+    ContactFormData data = formData.get();
+    System.out.println(data.firstName + " " + data.lastName + " " + data.telephone);
+    return ok(NewContact.render(formData));
   }
 }
---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,16 @@
+package views.formdata;
+
+/**
+ * Java backing class for form data.
+ * @author Alvin Wang
+ *
+ */
+public class ContactFormData {
+  /** The first name. */
+  public String firstName = "";
+  /** The last name. */
+  public String lastName = "";
+  /** The telephone number. */
+  public String telephone = "";
+
+}
