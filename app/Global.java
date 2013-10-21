e6d1f4a1f9e54db409637d82b93d859ffcd8cf4c
alvinwkwang <wang.wk.alvin@gmail.com>
2013-10-21 04:32:14 -1000

    Added initialization on startup.

---
+++ app/Global.java
@@ -0,0 +1,24 @@
+import models.ContactDB;
+import play.Application;
+import play.GlobalSettings;
+import views.formdata.ContactFormData;
+
+/**
+ * Provide initialization code for the digits application.
+ * @author Alvin Wang
+ *
+ */
+public class Global extends GlobalSettings {
+  
+  /**
+   * Initialize the system with some sample contacts.
+   * @param app The application.
+   */
+  public void onStart(Application app) {
+    ContactDB.addContact(new ContactFormData("Alvin", "Wang", "808-111-2222", "Home"));
+    ContactDB.addContact(new ContactFormData("John", "Smith", "123-456-7890", "Work"));
+    ContactDB.addContact(new ContactFormData("Jane", "Smith", "555-555-5555", "Mobile"));
+    ContactDB.addContact(new ContactFormData("Bat", "Man", "000-000-0000", "Work"));
+    ContactDB.addContact(new ContactFormData("Super", "Man", "322-725-8364", "Mobile"));
+  }
+}
