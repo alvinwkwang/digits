503ed68e992afafff0f5e7163bf55375f91122cc
alvinwkwang <wang.wk.alvin@gmail.com>
2013-12-08 21:42:55 -1000

    Starting WOD

--- app/Global.java
+++ app/Global.java
@@ -22,7 +22,7 @@ public class Global extends GlobalSettings {
     String adminEmail = Play.application().configuration().getString("digits.admin.email");
     String adminPassword = Play.application().configuration().getString("digits.admin.password");
     
-    UserInfoDB.defineAdmin("Administrator", adminEmail, adminPassword);
+    UserInfoDB.defineAdmin("Administrator", adminEmail, adminPassword); 
     
     if (UserInfoDB.adminDefined() && UserInfoDB.getUser(adminEmail).getContacts().isEmpty()) {
       ContactDB.addContact(adminEmail, new ContactFormData("Bruce", "Wayne", "123-456-7890", "Home"));
